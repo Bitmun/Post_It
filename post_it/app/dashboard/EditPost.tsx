0a5e11d8876ec7b6db534be293321035565833c1
Bitmun <pashabalko21@gmail.com>
2023-07-26 14:17:49 +0300

    add EditPost

--- post_it/app/dashboard/EditPost.tsx
+++ post_it/app/dashboard/EditPost.tsx
@@ -1,1 +1,40 @@
 "use client"
+
+import Image from "next/image"
+
+type EditProps = {
+  id: string
+  avatar: string
+  name: string
+  title: string
+  comments? : {
+    id: string
+    postId: string
+    userId: string
+  }[]
+}
+
+export default function EditPost({avatar, name, title, comments, id} : EditProps) {
+  return (
+    <div className=" bg-gray-200 my-8 p-8 rounded-lg">
+      <div className="flex gap-4 items-center">
+        <Image
+        height = {32}
+        width = {32}
+        src = {avatar}
+        alt="avatar"
+        />
+        <h3 className="font-bold text-gray-600">{name}</h3>
+      </div>
+      <div className="my-8">
+        <p className="break-all">{title}</p>
+      </div>
+      <div className="flex items-center gap-4">
+        <p className="text-sm font-bold text-gray-500">
+          {comments?.length} Comments
+        </p>
+        <button className="text-sm font-bold text-red-500">Delete</button>
+      </div>
+    </div>
+  )
+}
